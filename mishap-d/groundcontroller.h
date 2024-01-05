3a24ea3e93f0092b6fba4a860bd18fbfe13be8ae
rivques <38469076+rivques@users.noreply.github.com>
2024-01-05 14:05:23 -0500

    more skeletonizing

--- mishap-d/groundcontroller.h
+++ mishap-d/groundcontroller.h
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "mishapprotocol.h"
 
 void groundsetup(){
 
--- mishap-d/mishapprotocol.h
+++ mishap-d/mishapprotocol.h
@@ -1,11 +1,19 @@
+// this file mainly holds the shared LoRa protocol
+// also a few other shared structs
+
 #include <Arduino.h>
 
 struct ThreadedLogMsg {
   int level;
   String msg;
 };
+struct Vector3d {
+  float x;
+  float y;
+  float z;
+}; // 12 bytes
 
-enum PacketType : char {
+enum PacketType : byte {
   LaserAngle,
   TargetSettings,
   ClearedCache,
@@ -15,5 +23,26 @@ enum PacketType : char {
 
 struct MishapProtocolPacket {
   PacketType packetType;
-  char data[250];
+  byte data[250];
 };
+
+// packet structs
+struct LaserAngleData {
+  float theta;
+  float phi;
+}; // 8 bytes
+
+struct TargetSettingsData {
+  Vector3d targetPos;
+}; // 12 bytes
+
+struct ClearedCacheData {}; // 0 bytes
+
+struct PayloadTelemetryData {
+  unsigned long time; // time in milliseconds since payload power-on
+  float altitude;
+  Vector3d payloadPos;
+  Vector3d payloadVel;
+  Vector3d payloadImpactPos;
+  bool payload_dropped;
+}; // 45 bytes
