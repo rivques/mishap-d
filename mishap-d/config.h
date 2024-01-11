e743a27730760deed6a2923cc94f46b7a9b50d9c
rivques <38469076+rivques@users.noreply.github.com>
2024-01-11 10:04:15 -0500

    set up config.h

---
+++ mishap-d/config.h
@@ -0,0 +1,7 @@
+// configuration of everything
+#define STARTING_GROUND_STATION_LOCATION Vector3d{0, 0, 1} // meters
+#define WIND_ACCELERATION Vector3d{0, 0, 0} // m/s^2
+#define ACCEPTABLE_TARGET_ERROR 1 // meters
+
+// constants
+#define PI 3.141592
